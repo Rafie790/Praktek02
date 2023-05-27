900dc87b2eb41e70f33a84a44433f4e58610341a
Rafie790 <raficaesar3@gmail.com>
2023-05-27 23:44:46 +0800

    Constructor dengan parameter

---
+++ src/praktek02/RectangleAksi.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek02;
+
+/**
+ *
+ * @author Hanzo
+ */
+public class RectangleAksi {
+    public static void main(String[] args) {
+        Rectangle r1 = new Rectangle();
+        r1.panjang = 9;
+        r1.lebar = 3;
+        
+        r1.cetakInfo();
+        System.out.println("luas rectangle = "+r1.hitungLuas());
+        r1.cetakLuas();
+        
+        Rectangle r2 = new Rectangle();
+        r2.cetakInfo();
+        
+        Rectangle r3 = new Rectangle(50,30);
+        r3.cetakInfo();
+    }
+    
+}
