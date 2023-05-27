0edac3fefb2b77de077693d2bc6023ffa38b7a2c
Rafie790 <raficaesar3@gmail.com>
2023-05-27 23:10:13 +0800

    Membuat Atribut dan Behavior

---
+++ src/praktek02/Rectangle.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package praktek02;
+
+/**
+ *
+ * @author Hanzo
+ */
+public class Rectangle {
+    double panjang;
+    double lebar;
+    
+    void cetakInfo (){
+        System.out.println("====================");
+        System.out.println("Panjang : "+panjang);
+        System.out.println("Lebar : "+lebar);
+        System.out.println("====================");
+    }
+    
+}
